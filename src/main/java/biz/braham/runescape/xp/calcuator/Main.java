a572161385f1937c92207afb7ca5f3a082d690c8
Steven Braham <steven@braham.biz>
2017-04-20 00:40:01 +0200

    init

---
+++ src/main/java/biz/braham/runescape/xp/calcuator/Main.java
@@ -0,0 +1,13 @@
+package biz.braham.runescape.xp.calcuator;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: stevenbraham
+ * Date: 20-04-17
+ * Time: 00:33
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
